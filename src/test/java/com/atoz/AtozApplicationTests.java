9e17ceb4efca584578556a1e0d2ba9ba04b5beb1
SangMin90 <everminelee@naver.com>
2022-10-20 11:34:28 +0900

    [#1] 프로젝트 생성

---
+++ src/test/java/com/atoz/AtozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
