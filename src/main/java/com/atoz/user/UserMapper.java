76067564e5e6ae076c3349a8b97a494ce255e851
KSJeong761 <25474621+KSJeong761@users.noreply.github.com>
2022-10-23 15:24:37 +0900

    [#2] MySQL, MyBatis 연동

---
+++ src/main/java/com/atoz/user/UserMapper.java
@@ -0,0 +1,9 @@
+package com.atoz.user;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+    void addUser(User user);
+}
--- src/main/java/com/atoz/user/UserRepository.java
+++ src/main/java/com/atoz/user/UserRepository.java
@@ -2,6 +2,7 @@ package com.atoz.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,7 +10,12 @@ import org.springframework.stereotype.Repository;
 @Slf4j
 public class UserRepository {
 
+    @Autowired
+    private final UserMapper userMapper;
+
     public void addUser(User user) {
-        log.info("UserRepository addUser");
+        log.info("UserRepository addUser : " + user);
+
+        userMapper.addUser(user);
     }
 }
