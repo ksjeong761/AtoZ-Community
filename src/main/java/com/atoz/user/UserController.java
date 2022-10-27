cdbac7978722447d93563ca9892b7a3d22260ac1
KSJeong761 <25474621+KSJeong761@users.noreply.github.com>
2022-10-27 22:41:55 +0900

    [#2] 리뷰 반영 : Controller 관련 - 컨트롤러에 @RequestMapping 따로 안붙여도 됨 - 생성자 주입에는 @Autowired 따로 안붙여도 됨 - Controller 단위 테스트에는 MockMvc의 standalone setup 사용 제안 - @ModelAttribute보다는 @RequestBody 사용 제안

--- src/main/java/com/atoz/user/UserController.java
+++ src/main/java/com/atoz/user/UserController.java
@@ -3,10 +3,7 @@ package com.atoz.user;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -18,7 +15,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public String register(@ModelAttribute User user) {
+    public String register(@RequestBody User user) {
         log.info("UserController register");
 
         userService.register(user);
