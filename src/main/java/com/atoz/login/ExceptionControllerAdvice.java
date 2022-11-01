ac75f1f1647f09b1e1c9d75bafb0dbdc4867fbee
SangMin90 <everminelee@naver.com>
2022-11-01 14:15:40 +0900

    [#3] 피드백 반영 - ErrorResponse 클래스 생성 및 ExceptionHandler 보완

--- src/main/java/com/atoz/login/ExceptionControllerAdvice.java
+++ src/main/java/com/atoz/login/ExceptionControllerAdvice.java
@@ -1,11 +1,13 @@
 package com.atoz.login;
 
+import com.atoz.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,14 +16,36 @@ import java.util.Map;
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(LoginValidationException.class)
-    public ResponseEntity<Object> handleLoginValidation(LoginValidationException e) {
-        log.info("ExceptionControllerAdvice.handleLoginValidation");
+    public ResponseEntity<ErrorResponse> handleLoginValidation(HttpServletRequest request, LoginValidationException e) {
+        String remoteIp = getClientIP(request);
 
-        String errorMessage = e.getMessage();
-        Map<String, String> responseBodyMap = new HashMap<>();
-        responseBodyMap.put("message", errorMessage);
+        log.error("remoteIp={}, exception={}, errorMessage={}", remoteIp, e.getClass().getSimpleName(), e.getMessage());
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBodyMap);
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorMessage(e.getMessage()).build();
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 
+    private static String getClientIP(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null) {
+            ip = request.getRemoteAddr();
+        }
+
+        return ip;
+    }
 }
