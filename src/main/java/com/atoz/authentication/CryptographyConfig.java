38b4700ba5a811a7b788004ea59f7e078674e6af
SangMin90 <everminelee@naver.com>
2022-11-18 20:28:35 +0900

    [#3] 리뷰 반영

--- src/main/java/com/atoz/authentication/CryptographyConfig.java
+++ src/main/java/com/atoz/authentication/CryptographyConfig.java
@@ -3,11 +3,8 @@ package com.atoz.authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,22 +14,9 @@ public class CryptographyConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-
         String idForEncode = "argon2";
         Map encoders = new HashMap<>();
         encoders.put(idForEncode, new Argon2PasswordEncoder());
-
-        encoders.put("bcrypt", new BCryptPasswordEncoder());
-        encoders.put("ldap", new org.springframework.security.crypto.password.LdapShaPasswordEncoder());
-        encoders.put("MD4", new org.springframework.security.crypto.password.Md4PasswordEncoder());
-        encoders.put("MD5", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("MD5"));
-        encoders.put("noop", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance());
-        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
-        encoders.put("scrypt", new SCryptPasswordEncoder());
-        encoders.put("SHA-1", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-1"));
-        encoders.put("SHA-256",
-                new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-256"));
-        encoders.put("sha256", new org.springframework.security.crypto.password.StandardPasswordEncoder());
 
         return new DelegatingPasswordEncoder(idForEncode, encoders);
     }
