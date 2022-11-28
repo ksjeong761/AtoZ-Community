e2c35e62398c5a455e23e4036ee51b713be0d8c8
KSJeong761 <25474621+KSJeong761@users.noreply.github.com>
2022-11-28 22:24:04 +0900

    [#3] TokenParser가 JWT를 파싱하는 것 외의 동작을 하지 않도록 변경

--- src/main/java/com/atoz/security/authorization/JwtAuthorizationProvider.java
+++ src/main/java/com/atoz/security/authorization/JwtAuthorizationProvider.java
@@ -4,39 +4,90 @@ import com.atoz.error.exception.InvalidTokenException;
 import com.atoz.security.token.RefreshTokenEntity;
 import com.atoz.security.token.RefreshTokenMapper;
 import com.atoz.security.token.TokenParser;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @Component
 public class JwtAuthorizationProvider implements AuthorizationProvider {
 
+    private static final String AUTHORITIES_KEY = "auth";
+
     private final TokenParser tokenParser;
     private final RefreshTokenMapper refreshTokenMapper;
+    private final UserDetailsService userDetailsService;
 
     // 사용자 아이디에 해당하는 리프레시 토큰이 서버에 저장되어 있다면 유효한 토큰이다.
     public Authentication authorize(String jwt) {
-        Authentication authentication = tokenParser.parseAuthentication(jwt);
+        Claims claims = tokenParser.parseClaims(jwt);
+        String userId = claims.getSubject();
 
-        RefreshTokenEntity refreshTokenEntity = refreshTokenMapper.findTokenByKey(authentication.getName())
-                .orElseThrow(() -> new InvalidTokenException("로그아웃된 사용자입니다."));
-        if (!refreshTokenEntity.getTokenValue().equals(jwt)) {
+        Optional<RefreshTokenEntity> refreshTokenEntity = refreshTokenMapper.findTokenByKey(userId);
+        if (refreshTokenEntity.isEmpty()) {
+            throw new InvalidTokenException("로그아웃된 사용자입니다.");
+        }
+        if (!refreshTokenEntity.get().getTokenValue().equals(jwt)) {
             throw new InvalidTokenException("토큰이 일치하지 않습니다.");
         }
 
-        return authentication;
+        return buildAuthentication(claims);
     }
 
     public Authentication authorize(String accessToken, String refreshToken) {
-        Authentication authentication = tokenParser.parseAuthentication(accessToken);
+        Claims claims = tokenParser.parseClaims(accessToken);
+        String userId = claims.getSubject();
 
-        RefreshTokenEntity refreshTokenEntity = refreshTokenMapper.findTokenByKey(authentication.getName())
-                .orElseThrow(() -> new InvalidTokenException("로그아웃된 사용자입니다."));
-        if (!refreshTokenEntity.getTokenValue().equals(refreshToken)) {
+        Optional<RefreshTokenEntity> refreshTokenEntity = refreshTokenMapper.findTokenByKey(userId);
+        if (refreshTokenEntity.isEmpty()) {
+            throw new InvalidTokenException("로그아웃된 사용자입니다.");
+        }
+        if (!refreshTokenEntity.get().getTokenValue().equals(refreshToken)) {
             throw new InvalidTokenException("토큰이 일치하지 않습니다.");
         }
 
-        return authentication;
+        return buildAuthentication(claims);
+    }
+
+    private Authentication buildAuthentication(Claims claims) {
+        var principal = buildPrincipal(claims);
+        var credentials = buildCredentials(claims);
+        var authorities = buildAuthorities(claims);
+
+        return new UsernamePasswordAuthenticationToken(principal, credentials, authorities);
+    }
+
+    private UserDetails buildPrincipal(Claims claims) {
+        return userDetailsService.loadUserByUsername(claims.getSubject());
+    }
+
+    private String buildCredentials(Claims claims) {
+        return "";
+    }
+
+    private Collection<? extends GrantedAuthority> buildAuthorities(Claims claims) {
+        Object authorities = claims.get(AUTHORITIES_KEY);
+        if (authorities == null) {
+            throw new RuntimeException("토큰에 권한 정보가 존재하지 않습니다.");
+        }
+        if (!StringUtils.hasText(authorities.toString())) {
+            throw new RuntimeException("사용자가 가진 권한이 없습니다.");
+        }
+
+        return Stream.of(authorities)
+                .map(String::valueOf)
+                .map(SimpleGrantedAuthority::new)
+                .toList();
     }
 }
--- src/main/java/com/atoz/security/token/TokenParser.java
+++ src/main/java/com/atoz/security/token/TokenParser.java
@@ -7,22 +7,12 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import java.security.Key;
-import java.util.Collection;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 public class TokenParser {
-
-    private static final String AUTHORITIES_KEY = "auth";
 
     private final Key secretKey;
 
@@ -48,18 +38,5 @@ public class TokenParser {
         } catch (Exception ex) {
             throw new InvalidTokenException("잘못된 토큰입니다.");
         }
-    }
-
-    public Authentication parseAuthentication(String jwt) {
-        Claims claims = this.parseClaims(jwt);
-        if (claims.get(AUTHORITIES_KEY) == null || !StringUtils.hasText(claims.get(AUTHORITIES_KEY).toString())) {
-            throw new RuntimeException("유저에게 아무런 권한이 없습니다.");
-        }
-
-        Collection<? extends GrantedAuthority> authorities = Stream.of(claims.get(AUTHORITIES_KEY))
-                .map(String::valueOf)
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
-        return new UsernamePasswordAuthenticationToken(claims.getSubject(), "", authorities);
     }
 }
