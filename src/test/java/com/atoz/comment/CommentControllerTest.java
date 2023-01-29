c08851ef512933e0ef192163c2d2b01866bd8d37
KSJeong761 <25474621+ksjeong761@users.noreply.github.com>
2023-01-30 01:10:14 +0900

    [#12] CommentController 테스트 보충

--- src/test/java/com/atoz/comment/CommentControllerTest.java
+++ src/test/java/com/atoz/comment/CommentControllerTest.java
@@ -83,4 +83,305 @@ class CommentControllerTest {
 
         resultActions.andExpect(status().isBadRequest());
     }
+
+    @Test
+    void loadComments_게시글_번호가_long타입_최대치를_넘기면_400을_응답한다() throws Exception {
+        String tooBigPostId = "9223372036854775808L";
+
+
+        ResultActions resultActions = sut.perform(
+                get("/comments?postId={postId}", tooBigPostId)
+        );
+
+
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @CustomWithMockUser(username = USER_ID)
+    @Test
+    void addComment_댓글_추가_요청에_성공한다() throws Exception {
+        AddCommentRequestDto addCommentRequestDto = AddCommentRequestDto.builder()
+                .postId(1)
+                .parentCommentId(1)
+                .content("testComment")
+                .build();
+
+
+        ResultActions resultActions = sut.perform(post("/comments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(addCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isOk());
+    }
+
+    @CustomWithMockUser(username = USER_ID)
+    @Test
+    void addComment_댓글_추가_요청을_보낸_사용자_아이디를_SecurityContextHolder에서_가져온다() throws Exception {
+        AddCommentRequestDto addCommentRequestDto = AddCommentRequestDto.builder()
+                .postId(1)
+                .parentCommentId(1)
+                .content("testComment")
+                .build();
+
+
+        sut.perform(post("/comments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(addCommentRequestDto)));
+
+
+        assertEquals(USER_ID, commentService.capturedUserId);
+    }
+
+    @CustomWithMockUser(username = USER_ID)
+    @Test
+    void addComment_게시글_번호가_누락되면_400을_응답한다() throws Exception {
+        AddCommentRequestDto addCommentRequestDto = AddCommentRequestDto.builder()
+                .parentCommentId(1)
+                .content("testComment")
+                .build();
+
+
+        ResultActions resultActions = sut.perform(post("/comments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(addCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @CustomWithMockUser(username = USER_ID)
+    @Test
+    void addComment_부모_댓글_아이디가_누락되면_기본값이_사용된다() throws Exception {
+        AddCommentRequestDto addCommentRequestDto = AddCommentRequestDto.builder()
+                .postId(1)
+                .content("testComment")
+                .build();
+
+
+        ResultActions resultActions = sut.perform(post("/comments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(addCommentRequestDto)));
+
+
+        AddCommentRequestDto defaultObject = new AddCommentRequestDto();
+        AddCommentRequestDto capturedObject = commentService.capturedAddCommentRequestDto;
+        assertEquals(defaultObject.getParentCommentId(), capturedObject.getParentCommentId());
+        resultActions.andExpect(status().isOk());
+    }
+
+    @CustomWithMockUser(username = USER_ID)
+    @Test
+    void addComment_댓글_내용이_누락되면_400을_응답한다() throws Exception {
+        AddCommentRequestDto addCommentRequestDto = AddCommentRequestDto.builder()
+                .postId(1)
+                .parentCommentId(1)
+                .build();
+
+
+        ResultActions resultActions = sut.perform(post("/comments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(addCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @CustomWithMockUser(username = USER_ID)
+    @Test
+    void addComment_댓글이_너무_길면_400을_응답한다() throws Exception {
+        String tooLongContent = this.generateString(512);
+        AddCommentRequestDto addCommentRequestDto = AddCommentRequestDto.builder()
+                .postId(1)
+                .parentCommentId(1)
+                .content(tooLongContent)
+                .build();
+
+
+        ResultActions resultActions = sut.perform(post("/comments")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(addCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateComment_댓글_수정_요청에_성공한다() throws Exception {
+        long commentId = 1;
+        UpdateCommentRequestDto updateCommentRequestDto = UpdateCommentRequestDto.builder()
+                .postId(1)
+                .userId("testUserId")
+                .content("testContent")
+                .build();
+
+
+        ResultActions resultActions = sut.perform(patch("/comments/{commentId}", commentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isOk());
+    }
+
+
+    @Test
+    void updateComment_댓글_아이디가_long_타입_최대치를_넘으면_400을_응답한다() throws Exception {
+        String tooBigCommentId = "9223372036854775808L";
+        UpdateCommentRequestDto updateCommentRequestDto = UpdateCommentRequestDto.builder()
+                .postId(1)
+                .userId("testUserId")
+                .content("testContent")
+                .build();
+
+
+        ResultActions resultActions = sut.perform(patch("/comments/{commentId}", tooBigCommentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateComment_게시글_번호가_누락되면_400을_응답한다() throws Exception {
+        long commentId = 1;
+        UpdateCommentRequestDto updateCommentRequestDto = UpdateCommentRequestDto.builder()
+                .userId("testUserId")
+                .content("testContent")
+                .build();
+
+
+        ResultActions resultActions = sut.perform(patch("/comments/{commentId}", commentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateComment_댓글_작성자_아이디가_누락되면_400을_응답한다() throws Exception {
+        long commentId = 1;
+        UpdateCommentRequestDto updateCommentRequestDto = UpdateCommentRequestDto.builder()
+                .postId(1)
+                .content("testContent")
+                .build();
+
+
+        ResultActions resultActions = sut.perform(patch("/comments/{commentId}", commentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateComment_댓글_내용이_누락되면_400을_응답한다() throws Exception {
+        long commentId = 1;
+        UpdateCommentRequestDto updateCommentRequestDto = UpdateCommentRequestDto.builder()
+                .postId(1)
+                .userId("testUserId")
+                .build();
+
+
+        ResultActions resultActions = sut.perform(patch("/comments/{commentId}", commentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateComment_댓글_내용이_511자를_초과하면_400을_응답한다() throws Exception {
+        String tooLongContent = this.generateString(512);
+        long commentId = 1;
+        UpdateCommentRequestDto updateCommentRequestDto = UpdateCommentRequestDto.builder()
+                .postId(1)
+                .userId("testUserId")
+                .content(tooLongContent)
+                .build();
+
+
+        ResultActions resultActions = sut.perform(patch("/comments/{commentId}", commentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateComment_댓글_작성자_아이디가_20자를_초과하면_400을_응답한다() throws Exception {
+        String tooLongUserId = this.generateString(21);
+        long commentId = 1;
+        UpdateCommentRequestDto updateCommentRequestDto = UpdateCommentRequestDto.builder()
+                .postId(1)
+                .userId(tooLongUserId)
+                .content("testContent")
+                .build();
+
+
+        ResultActions resultActions = sut.perform(patch("/comments/{commentId}", commentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updateCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void increaseLikeCount_좋아요_요청에_성공한다() throws Exception {
+        long commentId = 1;
+
+
+        ResultActions resultActions = sut.perform(patch("/comments/{commentId}/like_count", commentId));
+
+
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteComment_댓글_삭제_요청에_성공한다() throws Exception {
+        long commentId = 1;
+        DeleteCommentRequestDto deleteCommentRequestDto = DeleteCommentRequestDto.builder()
+                .userId("testUserId")
+                .build();
+
+
+        ResultActions resultActions = sut.perform(delete("/comments/{commentId}", commentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(deleteCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteComment_댓글_작성자_아이디가_20자를_초과하면_400을_응답한다() throws Exception {
+        String tooLongUserId = this.generateString(21);
+        long commentId = 1;
+        DeleteCommentRequestDto deleteCommentRequestDto = DeleteCommentRequestDto.builder()
+                .userId(tooLongUserId)
+                .build();
+
+
+        ResultActions resultActions = sut.perform(patch("/comments/{commentId}", commentId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(deleteCommentRequestDto)));
+
+
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    private String generateString(int size) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            stringBuilder.append('A');
+        }
+
+        return stringBuilder.toString();
+    }
 }
